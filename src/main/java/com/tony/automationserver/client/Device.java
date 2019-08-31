64c4bc5dd6237dbbe4fe9e7881943096fae05ce2
Tony <abouzaidan.tony@gmail.com>
2019-08-31 15:08:04 +0300

    Add connected property

--- src/main/java/com/tony/automationserver/client/Device.java
+++ src/main/java/com/tony/automationserver/client/Device.java
@@ -2,6 +2,7 @@ package com.tony.automationserver.client;
 
 import java.util.HashMap;
 
+import com.tony.automationserver.sqlhelper.SQLHelper.SQLTypes;
 import com.tony.automationserver.sqlhelper.annotation.ManyToOne;
 import com.tony.automationserver.sqlhelper.annotation.Property;
 import com.tony.automationserver.sqlhelper.annotation.Table;
@@ -11,6 +12,9 @@ public class Device extends Client {
 
     @Property(name = "device_key")
     public String deviceKey;
+
+    @Property(name = "connected", type = SQLTypes.Boolean)
+    public boolean connected;
 
     @ManyToOne(targetEntity = User.class, inverserdBy = "user_id")
     public User userClient;
